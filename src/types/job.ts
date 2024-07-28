24624d49b1cd2ba0f1888f71b18d561e253156a1
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2024-07-28 16:17:09 +0300

    Update
    Updated for project assets.

--- src/types/job.ts
+++ src/types/job.ts
@@ -1,3 +1,15 @@
 export interface Job {
-    id: number;
-  }
+  id: number;
+  company: string;
+  logo: string;
+  new: boolean;
+  featured: boolean;
+  position: string;
+  role: string;
+  level: string;
+  postedAt: string;
+  contract: string;
+  location: string;
+  languages: string[];
+  tools: string[];
+}
