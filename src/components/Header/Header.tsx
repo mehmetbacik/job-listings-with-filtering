ce2e007c4b2c6fa82d550b1370d4c6207375e09a
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2024-07-29 20:40:57 +0300

    Update
    Updated for Header.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import styles from './styles/Header.module.scss';
 
-const Header: React.FC = () => {
-  return <>Header</>;
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <h1>Job Listings</h1>
+    </header>
+  );
 };
 
 export default Header;
